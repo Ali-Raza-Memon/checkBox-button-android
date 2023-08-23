47de8a2650565c5ecacbe7bccaaf0b8d41088478
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-08-23 19:37:28 +0500

    Perform actions by button and uploading image

---
+++ app/src/main/java/com/example/task01/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.task01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        var newText = findViewById<TextView>(R.id.myText);
+//        var btn2    = findViewById<Button>(R.id.btn1);
+//        btn2.setOnClickListener(View.OnClickListener {
+//            newText.text = "Ahsanullah";
+//        })
+
+//        ImageView imageView = findViewById(R.id.imageView);
+        var myImage = findViewById<ImageView>(R.id.cat2);
+        myImage.setImageResource(R.drawable.cat);
+
+    }
+}
